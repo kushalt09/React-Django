263a47f9e5929fabcbae41e367058d9b514123a2
kushalt09 <kushaltimilsina09@gmail.com>
2024-10-18 17:41:52 +0545

    Added Note

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,4 +1,8 @@
-function NotFound(){
-    return <div>Not Found</div>
+function NotFound() {
+    return <div>
+        <h1>404 Not Found</h1>
+        <p>The page you're looking for doesn't exist!</p>
+    </div>
 }
+
 export default NotFound
